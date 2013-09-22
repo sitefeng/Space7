b8f512c9ec605797029352ec488f0f7518009bbd
sitefeng <Site@SiteFengs-MacBook-Air.local>
2013-09-22 19:53:05 -0400

    001- Empty Cocos2d template with CocosDenshion

---
+++ Space7/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  Space7
+//
+//  Created by Si Te Feng on 2013-09-22.
+//  Copyright Si Te Feng 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
