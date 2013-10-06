449f019c6b825ee3a53b9a620f256f5480840f67
sitefeng <fengsite@hotmail.com>
2013-10-06 06:53:13 -0400

    final update 1

--- Space7/HighscoresMainLayer.m
+++ Space7/HighscoresMainLayer.m
@@ -54,9 +54,9 @@
         
         
         //Creating the tappable 3 Icons on the right side
-        CCMenuItemImage *facebookIcon = [CCMenuItemImage itemWithNormalImage:@"facebookIcon.png" selectedImage:@"closeIcon.png" target:self selector:@selector(facebookIconPressed)];
+        CCMenuItemImage *facebookIcon = [CCMenuItemImage itemWithNormalImage:@"FacebookIconNormal.png" selectedImage:@"FacebookIconPressed.png" target:self selector:@selector(facebookIconPressed)];
         
-        [facebookIcon setScale:2];
+        [facebookIcon setScale:0.8];
         
         CCMenuItemImage *closeIcon = [CCMenuItemImage itemWithNormalImage:@"closeIcon.png" selectedImage:@"facebookIcon.png" target:self selector:@selector(closeIconPressed)];
         [closeIcon setScale:2];
